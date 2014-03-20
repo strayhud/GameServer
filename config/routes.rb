89b12d5d42d1134d06393eea32ddd8eda4d4a0d4
Steve Hudson <strayhud@gmail.com>
2014-03-19 23:46:50 -0400

    some mods

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ GameServer::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/news', to: 'static_pages#news', via: 'get'
   match '/signup',  to: 'users#new', via: 'get'
   match '/signin',  to: 'sessions#new', via: 'get'
   match '/signout', to: 'sessions#destroy', via: 'delete'
--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -8,7 +8,7 @@ namespace :db do
                  admin: true)
     99.times do |n|
       name  = Faker::Name.name
-      email = "example-#{n+1}@railstutorial.org"
+      email = "user#{n+1}@gmail.com"
       password  = "password"
       User.create!(name: name,
                    email: email,
